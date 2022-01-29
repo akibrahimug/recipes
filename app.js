3d6baab1302540d9c2adc18b11f178fa4b897698
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2022-01-29 11:37:25 +0000

    public

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//- 1. Add static middleware
+app.use('/static', express.static('public'))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
